ee2b3ca3fe50cb6cc834b9253aa47f84cbbd9e56
warvbell <wardvanbelle@gmail.com>
2022-01-06 09:45:20 +0100

    improve code readability and style language selection

--- js/main.js
+++ js/main.js
@@ -35,7 +35,10 @@ let characterTyped = 0;
 let quoteNo = 0;
 let timer = null;
 
-// on load events
+//------------------
+// ON LOAD EVENTS
+//------------------
+
 function InitGame() {
 
   input_area.focus();
@@ -55,6 +58,10 @@ function InitGame() {
 }
 
 window.addEventListener("load", InitGame, true);
+
+//--------------------
+// UTILITY FUNCTIONS
+//--------------------
 
 language_select.addEventListener('change', function () {
   language = language_select.options[language_select.selectedIndex].value;
@@ -86,6 +93,10 @@ function getRandom(arr, n) {
   }
   return result;
 }
+
+//------------------------
+// TEXT UPDATE + PROCESS
+//------------------------
 
 // Pick & show 20 random words
 function updateWords(num_words) {
@@ -131,10 +142,8 @@ function processCurrentText() {
     if (!timer_start) {
       if (CURR_MODE === "time") {
         timer = setInterval(countdownTimer, 1000);
-      } else if (CURR_MODE === "words") {
-        timer = setInterval(checkWords, 1000)
       } else {
-        timer = setInterval(checkQuote, 1000)
+        timer = setInterval(countupTimer, 1000)
       };
 
       timer_start = true
@@ -241,6 +250,10 @@ function processCurrentText() {
     
 }
 
+//---------------------
+//    START + RESET
+//---------------------
+
 function startGame() {
 
     resetValues();
@@ -273,6 +286,10 @@ function resetValues() {
     wpm_group.style.display = "none";
 }
 
+//----------------------
+//        TIMERS
+//---------------------
+
 function countdownTimer() {
     if (timeLeft > 0) {
       // decrease the current time left
@@ -290,7 +307,7 @@ function countdownTimer() {
     }
 }
 
-function checkWords() {
+function countupTimer() {
     // increase the time elapsed
     timeElapsed++;
  
@@ -298,13 +315,9 @@ function checkWords() {
     timer_text.textContent = timeElapsed + "s";
 }
 
-function checkQuote() {
-  // increase the time elapsed
-  timeElapsed++;
- 
-  // update the timer text
-  timer_text.textContent = timeElapsed + "s";
-}
+//----------------------
+// FINISH GAME
+//----------------------
 
 function finishGame() {
     // stop the timer
@@ -336,7 +349,9 @@ function restartGame() {
   InitGame();
 }
 
-// Automatic restart when typing tab
+//--------------------------------------
+// AUTO RESTART WHEN TAB + ENTER
+//--------------------------------------
 
 var restart_down = false
 
@@ -358,7 +373,9 @@ input_area.addEventListener("keydown", function(event) {
   }
 });
 
-// Mode selection
+//------------------
+// MODE SELECTION
+//------------------
 
 mode_group.addEventListener('click', event => {
   if (event.target.className === 'mode') {
@@ -409,3 +426,87 @@ mode_subgroups.addEventListener('click', event => {
   }
 });
 
+//------------------------
+// STYLE SELECTION MENU
+//------------------------
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("selection_menu");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
